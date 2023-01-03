e3a9647a025e15b20d0b78a9ca3d7a8b76cd777c
JustinLung <justinlung77@gmail.com>
2023-01-03 20:12:01 +0100

    file structure updated

--- src/routes/booking/+page.svelte
+++ src/routes/booking/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import PageTransition from '$lib/components/PageTransition.svelte';
-    import BookingSection from '$lib/components/bookingPageComponents/BookingSection.svelte';
+	import BookingSection from '$lib/components/bookingPageComponents/BookingSection.svelte';
 </script>
 
 <PageTransition>
-    <BookingSection>
-        
-    </BookingSection>
+	<BookingSection />
 </PageTransition>
