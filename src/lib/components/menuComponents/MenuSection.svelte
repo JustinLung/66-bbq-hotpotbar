e3886a12579f5a38174ee767a2a094bc4837c966
JustinLung <justinlung77@gmail.com>
2023-01-03 20:43:53 +0100

    changed max-width on menu-section

---
+++ src/lib/components/menuComponents/MenuSection.svelte
@@ -0,0 +1,4 @@
+<section class="max-w-2xl mx-auto mt-10 flex flex-col justify-center gap-5">
+	<h1 class="text-3xl font-semibold text-center">Our Menu</h1>
+	<slot />
+</section>
--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import PageTransition from '$lib/components/PageTransition.svelte';
+	import MenuSection from '$lib/components/menuComponents/MenuSection.svelte';
 	const pageTitle = 'Menu';
 </script>
 
@@ -8,8 +9,7 @@
 </svelte:head>
 
 <PageTransition>
-	<section class="max-w-6xl mx-auto mt-10 flex flex-col justify-center gap-5">
-		<h1 class="text-3xl font-semibold text-center">Our Menu</h1>
+	<MenuSection>
 		<img src="/assets/images/menu/menu-page-1.jpeg" alt="Menu Page 1" />
 		<img src="/assets/images/menu/menu-page-2.jpeg" alt="Menu Page 2" />
 		<img src="/assets/images/menu/menu-page-3.jpeg" alt="Menu Page 3" />
@@ -17,5 +17,5 @@
 		<img src="/assets/images/menu/menu-page-5.jpeg" alt="Menu Page 5" />
 		<img src="/assets/images/menu/menu-page-6.jpeg" alt="Menu Page 6" />
 		<img src="/assets/images/menu/menu-page-7.jpeg" alt="Menu Page 7" />
-	</section>
+	</MenuSection>
 </PageTransition>
