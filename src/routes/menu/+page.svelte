8d20ae3ace2ce5939d069aab24b717073313f706
JustinLung <justinlung77@gmail.com>
2023-01-03 18:49:38 +0100

    added page transition to menu page

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,10 +1,16 @@
-<section class="max-w-6xl mx-auto mt-10 flex flex-col justify-center gap-5">
-	<h1 class="text-3xl font-semibold text-center">Our Menu</h1>
-	<img src="/assets/images/menu/menu-page-1.jpeg" alt="Menu Page 1" />
-	<img src="/assets/images/menu/menu-page-2.jpeg" alt="Menu Page 2" />
-	<img src="/assets/images/menu/menu-page-3.jpeg" alt="Menu Page 3" />
-	<img src="/assets/images/menu/menu-page-4.jpeg" alt="Menu Page 4" />
-	<img src="/assets/images/menu/menu-page-5.jpeg" alt="Menu Page 5" />
-	<img src="/assets/images/menu/menu-page-6.jpeg" alt="Menu Page 6" />
-	<img src="/assets/images/menu/menu-page-7.jpeg" alt="Menu Page 7" />
-</section>
+<script lang="ts">
+	import PageTransition from '$lib/components/PageTransition.svelte';
+</script>
+
+<PageTransition>
+	<section class="max-w-6xl mx-auto mt-10 flex flex-col justify-center gap-5">
+		<h1 class="text-3xl font-semibold text-center">Our Menu</h1>
+		<img src="/assets/images/menu/menu-page-1.jpeg" alt="Menu Page 1" />
+		<img src="/assets/images/menu/menu-page-2.jpeg" alt="Menu Page 2" />
+		<img src="/assets/images/menu/menu-page-3.jpeg" alt="Menu Page 3" />
+		<img src="/assets/images/menu/menu-page-4.jpeg" alt="Menu Page 4" />
+		<img src="/assets/images/menu/menu-page-5.jpeg" alt="Menu Page 5" />
+		<img src="/assets/images/menu/menu-page-6.jpeg" alt="Menu Page 6" />
+		<img src="/assets/images/menu/menu-page-7.jpeg" alt="Menu Page 7" />
+	</section>
+</PageTransition>
