77672b85ec0ab7a665937ec0530ca6346669924c
Justin Lung <justinlung77@gmail.com>
2025-01-06 15:59:08 +0100

    fixed styling

--- src/lib/components/homePageComponents/Dish.svelte
+++ src/lib/components/homePageComponents/Dish.svelte
@@ -6,8 +6,8 @@
 
 <article class="w-full bg-base-100 shadow-xl">
 	<figure>
-		<img src={dishImage} alt={dishTitle} class="w-full" />
-		<div class="bg-darkPurple p-5 rounded md:h-60">
+		<img src={dishImage} alt={dishTitle} class="w-full md:min-h-[15rem] object-cover" />
+		<div class="bg-darkPurple p-5 rounded md:h-full">
 			<h2 class="text-2xl font-semibold">{dishTitle}</h2>
 			<p>{dishDescription}</p>
 		</div>
